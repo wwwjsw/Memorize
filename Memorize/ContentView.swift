ac1847e4aad8fcefa2f55177e52c71606893f0f5
Guilherme henrique machado <progr.guilherme@gmail.com>
2020-11-09 23:34:35 -0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Guilherme Machado on 09/11/20.
+//  Copyright © 2020 Guilherme Machado. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
