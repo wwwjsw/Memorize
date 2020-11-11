59c6ed48a10ac5b9c6e68705827923fbe18df080
Guilherme henrique machado <progr.guilherme@gmail.com>
2020-11-10 23:56:49 -0300

    Func: get clicked card

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    var viewModel: EmojiMemoryGame
+    
     var body: some View {
-        Text("Hello, World!")
+        HStack(spacing: 10) {
+            ForEach(viewModel.cards) { card in
+                CardView(card: card).onTapGesture {
+                    self.viewModel.choose(card: card)
+                }
+            }
+        }
+        .foregroundColor(Color.orange)
+        .padding()
+        .font(Font.largeTitle)
     }
 }
 
+struct CardView: View {
+    var card: MemoryGame<String>.Card
+    
+    var body: some View {
+        ZStack {
+            if card.isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text(card.content)
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(viewModel: EmojiMemoryGame())
     }
 }
