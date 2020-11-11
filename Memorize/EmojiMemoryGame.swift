23d01683e888a7195e1537c542011e8131df79ea
Guilherme henrique machado <progr.guilherme@gmail.com>
2020-11-11 11:03:06 -0300

    Func: add intents and observables

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -8,8 +8,8 @@
 
 import SwiftUI
 
-class EmojiMemoryGame {
-    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+class EmojiMemoryGame: ObservableObject {
+    @Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
     
     static func createMemoryGame() -> MemoryGame<String> {
         let emojis: Array<String> = ["👻", "🔎", "🕸", "💡"]
@@ -17,6 +17,7 @@ class EmojiMemoryGame {
                 return emojis[pairIndex]
         }
     }
+    
     // MARK: - Access to the Model
     
     var cards: Array<MemoryGame<String>.Card> {
